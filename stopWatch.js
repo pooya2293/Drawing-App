0c1520081f32756ecaaf02d628850bb60842e916
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-01-27 12:04:55 +0330

    drawing 2 line in canvas for make setting of thickness join and cap

--- stopWatch.js
+++ stopWatch.js
@@ -11,4 +11,27 @@ $(function(){
         }
     });
 
+    // make canvas paintable
+    var canvas = document.getElementById("paint");
+    var context = canvas.getContext('2d');
+
+    // draw a line
+    // declare new path
+    context.beginPath();
+    // set line width
+    context.lineWidth = 30;
+    // set line color
+    context.strokeStyle = "#20b862";
+    // set cap to the line (round,butt,square)
+    context.lineCap = "round";
+    //set line join style (bevel,round,miter)
+    context.lineJoin = "round";
+    //positicon context point
+    context.moveTo(30,50);
+    //draw a straight line from staring point to a new position
+    context.lineTo(300,200);
+    //draw another line
+    context.lineTo(400,100);
+    // make line visible
+    context.stroke();
 });
